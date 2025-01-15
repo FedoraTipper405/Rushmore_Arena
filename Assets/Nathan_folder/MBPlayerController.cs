08faf7ed57da43f8aaf2027186b6d6d31ac92ed5
altorook0542 <nathanberry403@gmail.com>
2025-01-15 10:01:34 -0700

    Test Scene created

---
+++ Assets/Nathan_folder/MBPlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MBPlayerController : MonoBehaviour
+{
+    [SerializeField] float playerMoveSpeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
